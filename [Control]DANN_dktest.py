6113408f37fd4f1aa9027c051eb41533dbb3db13
dongjaekim <dongjaekim@dankook.ac.kre>
2024-08-01 02:31:58 +0900

    add to run

---
+++ [Control]DANN_dktest.py
@@ -0,0 +1,253 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from functions import ReverseLayerF, lr_lambda
+from data_loader import data_loader
+import numpy as np
+import wandb
+import time
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class DANN(nn.Module):
+    def __init__(self):
+        super(DANN, self).__init__()
+        self.restored = False
+
+        self.feature = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5),  # 28
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # 13
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5),  # 9
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # 4
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4),  # 1
+            nn.ReLU()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 1 * 1, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 10)
+        )
+
+        self.discriminator = nn.Sequential(
+            nn.Linear(128 * 1 * 1, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 2)
+        )
+
+    def forward(self, input_data, alpha=1.0):
+        input_data = input_data.expand(input_data.data.shape[0], 3, 32, 32)
+        feature = self.feature(input_data)
+        feature = feature.view(-1, 128 * 1 * 1)
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
+        class_output = self.classifier(feature)
+        domain_output = self.discriminator(reverse_feature)
+        return class_output, domain_output
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epoch', type=int, default=1000)
+    parser.add_argument('--pretrain_epoch', type=int, default=30)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--source', type=str, default='SVHN')
+    parser.add_argument('--target', type=str, default='MNIST')
+    parser.add_argument('--lr_cls', type=float, default=0.01)
+    parser.add_argument('--lr_dom', type=float, default=0.03)
+    args = parser.parse_args()
+
+    pre_epochs = args.pretrain_epoch
+    num_epochs = args.epoch
+
+    source_loader, source_loader_test = data_loader(args.source, args.batch_size)
+    target_loader, target_loader_test = data_loader(args.target, args.batch_size)
+
+    print("Data load complete, start training")
+
+    model = DANN().to(device)
+
+    pre_opt = optim.Adam(model.parameters(), lr=0.01)
+    optimizer_cls = optim.SGD(list(model.feature.parameters()) + list(model.classifier.parameters()), lr=args.lr_cls, momentum=0.9, weight_decay=1e-6)
+    optimizer_dom = optim.SGD(list(model.feature.parameters()) + list(model.discriminator.parameters()), lr=args.lr_dom, momentum=0.9, weight_decay=1e-6)
+    scheduler_cls = optim.lr_scheduler.LambdaLR(optimizer_cls, lr_lambda)
+    scheduler_dom = optim.lr_scheduler.LambdaLR(optimizer_dom, lr_lambda)
+    criterion = nn.CrossEntropyLoss()
+
+    # Initialize Weights and Biases
+    wandb.init(project="EM_Domain_pretrain_twoOptimizers",
+               entity="hails",
+               config=args.__dict__,
+               name="DANN:lr_cls" + str(args.lr_cls) + "lr_dom" + str(args.lr_dom) + "_Batch:" + str(args.batch_size)
+               )
+
+    for epoch in range(pre_epochs):
+        model.train()
+        i = 0
+
+        for source_data in source_loader:
+            p = (float(i + epoch * len(source_loader)) / num_epochs / len(source_loader))
+            lambda_p = 2. / (1. + np.exp(-10 * p)) - 1
+
+            # Training with source data
+            source_images, source_labels = source_data
+            source_images, source_labels = source_images.to(device), source_labels.to(device)
+
+            class_output, _ = model(source_images, alpha=lambda_p)
+            loss = criterion(class_output, source_labels)
+
+            pre_opt.zero_grad()
+            loss.backward()
+            pre_opt.step()
+
+            label_acc = (torch.argmax(class_output, dim=1) == source_labels).sum().item() / source_labels.size(0)
+            print(f'Batches [{i + 1}/{len(source_loader)}], '
+                  f'Pretrain Loss: {loss.item():.4f}, '
+                  f'Pretrain Accuracy: {label_acc * 100:.3f}%, '
+                  )
+
+            i += 1
+
+        # scheduler_cls.step()
+        # scheduler_dom.step()
+        model.eval()
+
+    for epoch in range(num_epochs):
+        start_time = time.time()
+
+        model.train()
+        i = 0
+
+        loss_tgt_domain_epoch = 0
+        loss_src_domain_epoch = 0
+        loss_label_epoch = 0
+
+        for source_data, target_data in zip(source_loader, tqdm(target_loader)):
+            p = (float(i + epoch * min(len(source_loader), len(target_loader))) /
+                 num_epochs / min(len(source_loader), len(target_loader)))
+            lambda_p = 2. / (1. + np.exp(-10 * p)) - 1
+
+            # Training with source data
+            source_images, source_labels = source_data
+            source_images, source_labels = source_images.to(device), source_labels.to(device)
+
+            class_output, _ = model(source_images, alpha=lambda_p)
+            label_loss = criterion(class_output, source_labels)
+
+            optimizer_cls.zero_grad()
+            optimizer_dom.zero_grad()
+
+            loss_label_epoch += label_loss.item()
+
+            _, domain_output = model(source_images, alpha=lambda_p)
+            source_dlabel = torch.full((source_images.size(0),), 1, dtype=torch.long, device=device)
+
+            domain_src_loss = criterion(domain_output, source_dlabel)
+
+            loss_src_domain_epoch += domain_src_loss.item()
+
+            # Training with target data
+            target_images, _ = target_data
+            target_images = target_images.to(device)
+
+            _, domain_output = model(target_images, alpha=lambda_p)
+            target_dlabel = torch.full((target_images.size(0),), 0, dtype=torch.long, device=device)
+
+            domain_tgt_loss = criterion(domain_output, target_dlabel)
+
+            loss  = domain_tgt_loss+domain_src_loss+label_loss
+
+            loss.backward()
+            optimizer_cls.step()
+            optimizer_dom.step()
+
+            loss_tgt_domain_epoch += domain_tgt_loss.item()
+
+            label_acc = (torch.argmax(class_output, dim=1) == source_labels).sum().item() / source_labels.size(0)
+            domain_acc = (torch.argmax(domain_output, dim=1) == target_dlabel).sum().item() / target_dlabel.size(0)
+
+            print(f'Batches [{i + 1}/{min(len(source_loader), len(target_loader))}], '
+                  f'Domain source Loss: {domain_src_loss.item():.4f}, '
+                  f'Domain target Loss: {domain_tgt_loss.item():.4f}, '
+                  f'Label Loss: {label_loss.item():.4f}, '
+                  f'Label Accuracy: {label_acc * 100:.3f}%, '
+                  f'Domain Accuracy: {domain_acc * 100:.3f}%')
+
+            i += 1
+
+        scheduler_cls.step()
+        scheduler_dom.step()
+
+        end_time = time.time()
+        print(f'Epoch [{epoch + 1}/{num_epochs}], '
+              f'Domain source Loss: {loss_src_domain_epoch:.4f}, '
+              f'Domain target Loss: {loss_tgt_domain_epoch:.4f}, '
+              f'Label Loss: {loss_label_epoch:.4f}, '
+              f'Total Loss: {loss_src_domain_epoch + loss_tgt_domain_epoch  + loss_label_epoch:.4f}, '
+              f'Time: {end_time - start_time:.2f} seconds'
+              )
+
+        wandb.log({
+            'Domain source Loss': loss_src_domain_epoch,
+            'Domain target Loss': loss_tgt_domain_epoch,
+            'Label Loss': loss_label_epoch,
+            'Total Loss': loss_src_domain_epoch + loss_tgt_domain_epoch + loss_label_epoch,
+            'Training Time': end_time - start_time
+        })
+
+        model.eval()
+
+        def lc_tester(loader, group):
+            correct, total = 0, 0
+            for images, labels in loader:
+                images, labels = images.to(device), labels.to(device)
+
+                class_output, _ = model(images, alpha=0.0)
+                preds = F.log_softmax(class_output, dim=1)
+
+                _, predicted = torch.max(preds.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+            accuracy = correct / total
+            wandb.log({'[Label] ' + group + ' Accuracy': accuracy}, step=epoch + 1)
+            print('[Label] ' + group + f' Accuracy: {accuracy * 100:.3f}%')
+
+        def dc_tester(loader, group, d_label):
+            correct, total = 0, 0
+            for images, _ in loader:
+                images = images.to(device)
+
+                _, domain_output = model(images, alpha=0.0)
+                preds = F.log_softmax(domain_output, dim=1)
+
+                _, predicted = torch.max(preds.data, 1)
+                total += images.size(0)
+                correct += (predicted == d_label).sum().item()
+
+            accuracy = correct / total
+            wandb.log({'[Domain] ' + group + ' Accuracy': accuracy}, step=epoch + 1)
+            print('[Domain] ' + group + f' Accuracy: {accuracy * 100:.3f}%')
+
+        with torch.no_grad():
+            lc_tester(source_loader_test, 'Source')
+            lc_tester(target_loader_test, 'Target')
+            dc_tester(source_loader_test, 'Source', 1)
+            dc_tester(target_loader_test, 'Target', 0)
+
+
+if __name__ == '__main__':
+    main()
