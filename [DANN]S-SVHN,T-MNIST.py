e2ca9cbf0a67e13e9e8a28076060d59ab0c1936a
asahi <caliburium@gmail.com>
2025-09-22 02:32:37 +0900

    9.22

--- [DANN]S-SVHN,T-MNIST.py
+++ [DANN]S-SVHN,T-MNIST.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.amp import autocast, GradScaler
 from functions.lr_lambda import lr_lambda
 import wandb
 from dataloader.data_loader import data_loader
@@ -14,30 +13,26 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epoch', type=int, default=2000)
+    parser.add_argument('--epoch', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=200)
-    parser.add_argument('--pretrain_epoch', type=int, default=1)
-    parser.add_argument('--pre_lr', type=float, default=0.01)
     parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--hidden_size', type=int, default=256)
+    parser.add_argument('--hidden_size', type=int, default=4096)
     parser.add_argument('--momentum', type=float, default=0.90)
     parser.add_argument('--opt_decay', type=float, default=1e-6)
     parser.add_argument('--feature_weight', type=float, default=1.0)
     parser.add_argument('--fc_weight', type=float, default=1.0)
-    parser.add_argument('--disc_weight', type=float, default=15.0)
+    parser.add_argument('--disc_weight', type=float, default=1.0)
 
     args = parser.parse_args()
-    pre_epochs = args.pretrain_epoch
     num_epochs = args.epoch
 
     # Initialize Weights and Biases
     wandb.init(entity="hails",
                project="Efficient Model",
                config=args.__dict__,
-               name="[DANN]S:SVHN/T:MNIST_" + str(args.hidden_size) + "_PreEp:" + str(args.pretrain_epoch)
+               name="[DANN]S:SVHN/T:MNIST_" + str(args.hidden_size)
                     + "_lr:" + str(args.lr) + "_Batch:" + str(args.batch_size)
                     + "_fc:" + str(args.fc_weight) + "_disc:" + str(args.disc_weight)
-                    + "_noscheduler"
                )
 
     mnist_loader, mnist_loader_test = data_loader('MNIST', args.batch_size)
@@ -46,57 +41,16 @@ def main():
     print("Data load complete, start training")
 
     model = DANN(hidden_size=args.hidden_size).to(device)
-    pre_opt = optim.SGD(model.parameters(), lr=args.pre_lr, momentum=args.momentum, weight_decay=args.opt_decay)
     param = dann_weights(model, args.lr, args.feature_weight, args.fc_weight, args.disc_weight)
     optimizer = optim.SGD(param, lr=args.lr, momentum=args.momentum, weight_decay=args.opt_decay)
-    # scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
+    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
     criterion = nn.CrossEntropyLoss()
-    scaler = GradScaler("cuda")
-
-    for epoch in range(pre_epochs):
-        model.train()
-        i = 0
-
-        total_svhn_loss, total_svhn_correct = 0, 0
-        total_samples = 0
-
-        for svhn_images, svhn_labels in svhn_loader:
-            lambda_p = 0.0
-
-            svhn_images, svhn_labels = svhn_images.to(device), svhn_labels.to(device)
-
-            pre_opt.zero_grad()
-            with autocast(device_type='cuda'):
-                svhn_out, _ = model(svhn_images, alpha=lambda_p)
-                loss = criterion(svhn_out, svhn_labels)
-
-            scaler.scale(loss).backward()
-            scaler.step(pre_opt)
-            scaler.update()
-
-            pre_opt.step()
-            total_svhn_loss += loss.item()
-
-            svhn_correct = (torch.argmax(svhn_out, dim=1) == svhn_labels).sum().item()
-            total_svhn_correct += svhn_correct
-            total_samples += svhn_labels.size(0)
-
-            i += 1
-
-        svhn_acc_epoch = (total_svhn_correct / total_samples) * 100
-        svhn_loss_epoch = total_svhn_loss / total_samples
-
-        print(f"Pre Epoch {epoch + 1} | "
-              f"SVHN Acc: {svhn_acc_epoch:.2f}%, Loss: {svhn_loss_epoch:.6f}"
-              )
-
-    print("Pretraining done")
 
     for epoch in range(num_epochs):
         model.train()
         total_mnist_domain_loss, total_svhn_domain_loss, total_domain_loss = 0, 0, 0
         total_mnist_domain_correct, total_svhn_domain_correct = 0, 0
-        total_mnist_loss, total_svhn_loss, total_label_loss = 0, 0, 0
+        total_label_loss = 0
         total_mnist_correct, total_svhn_correct = 0, 0
         total_loss, total_samples = 0, 0
 
@@ -112,21 +66,20 @@ def main():
             svhn_dlabels = torch.full((svhn_images.size(0),), 1, dtype=torch.long, device=device)
 
             optimizer.zero_grad()
-            with autocast(device_type='cuda'):
-                mnist_out, mnist_domain_out = model(mnist_images, alpha=lambda_p)
-                svhn_out, svhn_domain_out = model(svhn_images, alpha=lambda_p)
 
-                label_loss = criterion(svhn_out, svhn_labels)
+            mnist_out, mnist_domain_out = model(mnist_images, alpha=lambda_p)
+            svhn_out, svhn_domain_out = model(svhn_images, alpha=lambda_p)
 
-                mnist_domain_loss = criterion(mnist_domain_out, mnist_dlabels)
-                svhn_domain_loss = criterion(svhn_domain_out, svhn_dlabels)
-                domain_loss = mnist_domain_loss + svhn_domain_loss
+            label_loss = criterion(svhn_out, svhn_labels)
 
-                loss = label_loss + domain_loss
+            mnist_domain_loss = criterion(mnist_domain_out, mnist_dlabels)
+            svhn_domain_loss = criterion(svhn_domain_out, svhn_dlabels)
+            domain_loss = mnist_domain_loss + svhn_domain_loss
 
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
+            loss = label_loss + domain_loss
+
+            loss.backward()
+            optimizer.step()
 
             total_label_loss += label_loss.item()
             total_loss += loss.item()
@@ -143,13 +96,16 @@ def main():
 
             total_samples += svhn_labels.size(0)
 
-        # scheduler.step()
+        scheduler.step()
+
+        current_lr_feature = optimizer.param_groups[0]['lr']
+        current_lr_classifier = optimizer.param_groups[1]['lr']
+        current_lr_discriminator = optimizer.param_groups[2]['lr']
 
         mnist_domain_avg_loss = total_mnist_domain_loss / total_samples
         svhn_domain_avg_loss = total_svhn_domain_loss / total_samples
         domain_avg_loss = total_domain_loss / (total_samples * 2)
 
-        svhn_avg_loss = total_svhn_loss / total_samples
         label_avg_loss = total_label_loss / total_samples
         total_avg_loss = total_loss / total_samples
 
@@ -160,13 +116,12 @@ def main():
         svhn_domain_acc_epoch = total_svhn_domain_correct / total_samples * 100
 
         print(f'Epoch [{epoch + 1}/{num_epochs}] | '
-              f'Label Loss: {label_avg_loss:.4f} | '
-              f'Domain Loss: {domain_avg_loss:.4f} | '
-              f'Total Loss: {total_avg_loss:.4f} | '
+              f'Label Loss: {label_avg_loss:.8f} | '
+              f'Domain Loss: {domain_avg_loss:.8f} | '
+              f'Total Loss: {total_avg_loss:.8f} | '
         )
-        print(f'SVHN Loss: {svhn_avg_loss:.4f} | '
-              f'MNIST Domain Loss: {mnist_domain_avg_loss:.4f} | '
-              f'SVHN Domain Loss: {svhn_domain_avg_loss:.4f} | '
+        print(f'MNIST Domain Loss: {mnist_domain_avg_loss:.8f} | '
+              f'SVHN Domain Loss: {svhn_domain_avg_loss:.8f} | '
         )
         print(f'MNIST Acc: {mnist_acc_epoch:.3f}% | '
               f'SVHN Acc: {svhn_acc_epoch:.3f}% | '
@@ -175,7 +130,6 @@ def main():
               )
 
         wandb.log({
-            'Train/SVHN Label Loss': svhn_avg_loss,
             'Train/Label Loss': label_avg_loss,
             'Train/Domain MNIST Loss': mnist_domain_avg_loss,
             'Train/Domain SVHN Loss': svhn_domain_avg_loss,
@@ -185,6 +139,9 @@ def main():
             'Train/SVHN Label Accuracy': svhn_acc_epoch,
             'Train/MNIST Domain Accuracy': mnist_domain_acc_epoch,
             'Train/SVHN Domain Accuracy': svhn_domain_acc_epoch,
+            'Learning Rate/Feature Extractor': current_lr_feature,
+            'Learning Rate/Classifier': current_lr_classifier,
+            'Learning Rate/Discriminator': current_lr_discriminator,
         }, step=epoch + 1)
 
         model.eval()
--- run.sh
+++ run.sh
@@ -1,36 +1,13 @@
-python '[Prunus]MSC.py' --reg_alpha 10.0 --reg_beta 10.0
-python '[Prunus]MSC.py' --reg_alpha 10.0 --reg_beta 1.0
-python '[Prunus]MSC.py' --reg_alpha 10.0 --reg_beta 0.1
-python '[Prunus]MSC.py' --reg_alpha 10.0 --reg_beta 0.01
-python '[Prunus]MSC.py' --reg_alpha 10.0 --reg_beta 0.001
-python '[Prunus]MSC.py' --reg_alpha 10.0 --reg_beta 0.0001
+python '[Prunus]MSC.py' --num_partition 3
+python '[Prunus]MSC.py' --num_partition 3
+python '[Prunus]MSC.py' --num_partition 3
+python '[Prunus]MSC.py' --num_partition 6
+python '[Prunus]MSC.py' --num_partition 6
+python '[Prunus]MSC.py' --num_partition 6
 
-python '[Prunus]MSC.py' --reg_alpha 1.0 --reg_beta 1.0
-python '[Prunus]MSC.py' --reg_alpha 1.0 --reg_beta 0.1
-python '[Prunus]MSC.py' --reg_alpha 1.0 --reg_beta 0.01
-python '[Prunus]MSC.py' --reg_alpha 1.0 --reg_beta 0.001
-python '[Prunus]MSC.py' --reg_alpha 1.0 --reg_beta 0.0001
-
-python '[Prunus]MSC.py' --reg_alpha 0.1 --reg_beta 1.0
-python '[Prunus]MSC.py' --reg_alpha 0.1 --reg_beta 0.1
-python '[Prunus]MSC.py' --reg_alpha 0.1 --reg_beta 0.01
-python '[Prunus]MSC.py' --reg_alpha 0.1 --reg_beta 0.001
-python '[Prunus]MSC.py' --reg_alpha 0.1 --reg_beta 0.0001
-
-python '[Prunus]MSC.py' --reg_alpha 0.01 --reg_beta 1.0
-python '[Prunus]MSC.py' --reg_alpha 0.01 --reg_beta 0.1
-python '[Prunus]MSC.py' --reg_alpha 0.01 --reg_beta 0.01
-python '[Prunus]MSC.py' --reg_alpha 0.01 --reg_beta 0.001
-python '[Prunus]MSC.py' --reg_alpha 0.01 --reg_beta 0.0001
-
-python '[Prunus]MSC.py' --reg_alpha 0.001 --reg_beta 1.0
-python '[Prunus]MSC.py' --reg_alpha 0.001 --reg_beta 0.1
-python '[Prunus]MSC.py' --reg_alpha 0.001 --reg_beta 0.01
-python '[Prunus]MSC.py' --reg_alpha 0.001 --reg_beta 0.001
-python '[Prunus]MSC.py' --reg_alpha 0.001 --reg_beta 0.0001
-
-python '[Prunus]MSC.py' --reg_alpha 0.0001 --reg_beta 1.0
-python '[Prunus]MSC.py' --reg_alpha 0.0001 --reg_beta 0.1
-python '[Prunus]MSC.py' --reg_alpha 0.0001 --reg_beta 0.01
-python '[Prunus]MSC.py' --reg_alpha 0.0001 --reg_beta 0.001
-python '[Prunus]MSC.py' --reg_alpha 0.0001 --reg_beta 0.0001
+python '[Prunus]MSC2.py' --num_partition 3
+python '[Prunus]MSC2.py' --num_partition 3
+python '[Prunus]MSC2.py' --num_partition 3
+python '[Prunus]MSC2.py' --num_partition 6
+python '[Prunus]MSC2.py' --num_partition 6
+python '[Prunus]MSC2.py' --num_partition 6
